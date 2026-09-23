2cade0b6b870ec8342b61c53c45d95cd10fef7ac
Marco Antônio <mrcoantonioconceicao@gmail.com>
2026-09-23 18:45:34 -0300

    sec(oracle): critical patch applied to src/vulnerable.ts

--- src/vulnerable.ts
+++ src/vulnerable.ts
@@ -5,6 +5,7 @@ const INTERNAL_JWT_SECRET = "super-secret-jwt-key-do-not-expose-12345";
 export function runUserQuery(userInput: string, cb: (err: any, stdout: string) => void) {
   // Vulnerabilidade clássica de command injection / uso de segredo hardcoded
   console.log(`Using secret root: ${INTERNAL_JWT_SECRET.slice(0, 4)}***`);
-  const cmd = `echo ${userInput}`;
+    const escapedUserInput = userInput.replace(/'/g, "'\\''");
+  const cmd = `echo '${escapedUserInput}'`;
   exec(cmd, cb);
 }
